79fb9465b47adb8c45f2078c0023ef079c1369ab
TaflanRares <taflan.r@yahoo.com>
2026-02-11 19:18:59 +0200

    Language server and snippets fixes

--- lua/options.lua
+++ lua/options.lua
@@ -3,7 +3,24 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.opt.number = true
+vim.opt.wrap = false
+vim.opt.linebreak = false
+vim.opt.breakindent = false
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.opt.clipboard = "unnamedplus"
 vim.opt.background = "dark"
+
+vim.api.nvim_create_autocmd("VimLeave", {
+  callback = function()
+    vim.cmd("silent !cls")
+  end,
+})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() == 0 then
+      vim.cmd("Alpha")
+    end
+  end,
+})
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,38 +1,49 @@
-local langs = { "lua_ls", "rust_analyzer", "clangd" }
-return {
-  {
-    "mason-org/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = langs
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      vim.lsp.set_log_level("ERROR")
-      local servers = langs
-      vim.lsp.enable(servers)
-
-      -- keymaps
-      local bufopts = { noremap=true, silent=true }
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, bufopts)
-      vim.keymap.set({'n','v'}, '<leader>ca', vim.lsp.buf.code_action, bufopts)
-      vim.keymap.set('n', '<C-e>', function()
-        vim.diagnostic.open_float({ scope = "buffer" })
-      end, bufopts)
-      vim.keymap.set('i', '<C-e>', function()
-        vim.diagnostic.open_float({ scope = "line" })
-      end, bufopts)
-    end
-  }
+local langs = {
+	"lua_ls",
+	"rust_analyzer",
+	"clangd",
+	"jdtls",
+	"html",
 }
+return {
+	{
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = langs,
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			vim.lsp.set_log_level("ERROR") 
+			for _, server in ipairs(langs) do
+				vim.lsp.config(server, {
+					capabilities = capabilities,
+				})
+			end
 
+			vim.lsp.enable(langs)
+
+			-- keymaps
+			local bufopts = { noremap = true, silent = true }
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, bufopts)
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, bufopts)
+			vim.keymap.set("n", "<C-e>", function()
+				vim.diagnostic.open_float({ scope = "buffer" })
+			end, bufopts)
+			vim.keymap.set("i", "<C-e>", function()
+				vim.diagnostic.open_float({ scope = "line" })
+			end, bufopts)
+		end,
+	},
+}
