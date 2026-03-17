c7483778e7cd1cbb06e1c5fbd73433ca1e9f8855
TaflanRares <taflan.r@yahoo.com>
2026-03-17 15:05:47 +0200

    Filetype aware lspconfig, added verilog language server

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -4,40 +4,59 @@ local langs = {
 	"clangd",
 	"jdtls",
 	"html",
+	"verible",
 }
+
+local server_filetypes = {
+	lua_ls        = { "lua" },
+	rust_analyzer = { "rust" },
+	clangd        = { "c", "cpp", "objc", "objcpp" },
+	jdtls         = { "java" },
+	html          = { "html" },
+	verible       = { "systemverilog", "verilog" },
+}
+
 return {
 	{
 		"mason-org/mason.nvim",
-    cmd = "Mason",
+		cmd = "Mason",
 		config = function()
 			require("mason").setup()
 		end,
 	},
 	{
 		"mason-org/mason-lspconfig.nvim",
-    event = "BufReadPre",
+		event = "BufReadPre",
 		config = function()
-			require("mason-lspconfig").setup({
-				--NOTE:: not needed at every startup
-        --ensure_installed = langs,
-			})
+			require("mason-lspconfig").setup()
 		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
-    event = "BufReadPre",
+		event = "BufReadPre",
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			vim.lsp.set_log_level("ERROR")
+
 			for _, server in ipairs(langs) do
-				vim.lsp.config(server, {
-					capabilities = capabilities,
-				})
+				vim.lsp.config(server, { capabilities = capabilities })
 			end
 
-			vim.lsp.enable(langs)
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(args)
+					local ft = args.match
+					for server, filetypes in pairs(server_filetypes) do
+						for _, supported_ft in ipairs(filetypes) do
+							if ft == supported_ft then
+								vim.lsp.enable(server)
+								return
+							end
+						end
+					end
+				end,
+			})
 
-			-- keymaps
+			-- Keymaps
 			local bufopts = { noremap = true, silent = true }
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, bufopts)
